4ce181916e6c676e1fa5b071de8c0ed0635e6fff
JayBee <jemallbrown@gmail.com>
2018-11-26 18:25:01 -0500

    adding updates although still not working

--- index.js
+++ index.js
@@ -2,32 +2,64 @@
 
 //   * Randomly selects a word and uses the `Word` constructor to store it
 
-//   * Prompts the user for each guess and keeps track of the user's remaining guesses
-
+// the first files are the required NPM modules and external constructors
 const inquirer = require('inquirer');
 const fs = require('fs');
-const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-const userGuess = [];
-const wordArray = [];
-var guessesLeft = 0;
+const Word = require("./word")
+const Letter = require('./letter')
+const fullWord = []
 
+// This is the letters array to check against
+const lettersArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+// I am using different trees as words to guess
+const wordsToGuessArray = ['oak','juniper','maple','sequoia','magnolia','willow','dogwood','spruce','elm','cedar'];
+
+
+// array for the letters that have been guessed by player
+var lettersGuessedWrong = [];
+
+// an array to store correctly guessed letters;
+var correctLetterGuesses = [];
+
+var chooseAnotherWord = true;
+
+var wordCount = wordsToGuessArray.length;
+var guessesLeft = 10;
+var wordtoGuess = ""
+
+// this function selects a random word from the list of trees.  It can change automously based on the number of trees in the array.
+const selectRandomWord = function (){
+  var indexValue = (Math.floor(Math.random() * wordCount))
+  wordtoGuess = wordsToGuessArray[indexValue];
+  return wordtoGuess;
+}
+
+var word = new Word (selectRandomWord());
+
+// this function will clear the screen at the begining of game play
 console.reset = function () {
     return process.stdout.write('\033c');
   }
 
-const playGame = function(){
+
+const mainGamePlay = function(){
     // clearing the screen to make game play easier
     console.reset()
     // choose word to use
-    // setting up temp word to choose from till i get other functions working
-    var word = "wisdom"
-    var wordArray = word.split('')
-    console.log(word)
-    console.log("the word array is "+wordArray)
-    console.log(typeof wordArray)
+    if (chooseAnotherWord) {
+    selectRandomWord();
+      chooseAnotherWord = false;
+      word = new Word(wordtoGuess);
+    }
+
+
+    word.newLetterObjects.forEach(completeCheck);
 
     // give user twice as many guesses as the word length
-    guessesLeft = (word.length * 2)
+    guessesLeft = (wordtoGuess.length * 2)
+
+    if (fullWord.includes(false)){ 
 
     // Create a "Prompt" to begin the word guess application
     inquirer
@@ -40,10 +72,74 @@ const playGame = function(){
         },
       ])
       .then(function(response) {
-            console.log(response.guessedLetter)
-            console.log(guessesLeft)
-        }
-    );
-    };
 
-    playGame()
+            if (response === 'exit'){
+              process.exit();
+            }
+            else if (!lettersArray.includes(response.guessedLetter) || response.guessedLetter.length > 1){
+              console.log("\n")
+              consold.log("Wrong guess! Try another letter please!\n");
+              mainGamePlay();
+            } else {
+              if(lettersGuessedWrong.includes(response.guessedLetter) || correctLetterGuesses.includes(response.guessedLetter) || response.guessedLetter === ""){
+                console.log("\n")
+                console.log('You already guessed this letter\n')
+                mainGamePlay();
+              }
+              else {
+                var wordCheckArray = [];
+
+                word.storeUserGuess(response.guessedLetter);
+
+                //confirm if the guess is right
+                word.newLetterObjects.forEach(wordCheck);
+                if (wordCheckArray.join('') === fullWord.join('')) {
+                  console.log("\nWrong!!\n");
+
+                  lettersGuessedWrong.push(response.guessedLetter);
+
+                  guessesLeft--;
+                } else {
+                  console.log("\nCorrect!! Keep up the good work!\n")
+
+                  correctLetterGuesses.push(response.guessedLetter);
+
+                }
+                word.createWordArray();
+              
+
+              console.log("Guesses remaing: " + lettersGuessedWrong.join(" ") + "\n");
+
+              console.log("Letters Guessed: " + lettersGuessedWrong.join(" ") + "\n");
+
+              if(guessesLeft > 0){
+                mainGamePlay();
+              } else {
+                console.log("You're out of guesses. Game over!\n")
+
+                restartGame();
+              }
+
+              function wordCheck(key) {
+                wordCheckArray.push(key.guessedLetter)
+              }
+            }
+          }
+        })
+  } else {
+      console.log("Winner!!\n");
+
+      restartGame();
+  }
+  
+  function completeCheck(key){
+      fullWord.push(key.guessedLetter);
+  }
+}
+
+function restartGame(){
+  console.log('restart game selected')
+}
+
+    mainGamePlay()
+    
--- word.js
+++ word.js
@@ -1,19 +1,43 @@
-// * **Word.js**: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
+// * **Word.js**: Contains a constructor, Word that depends on the Letter constructor. 
+const Letter = require('./letter')
 
+//This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
 //   * An array of `new` Letter objects representing the letters of the underlying word
+const Word = function (wordToGuess){
+    console.log('in the main Word constructor function')
+    console.log('this.word is ' + wordToGuess)
+    this.word = wordToGuess;
+    console.log('the word is ' + this.word)
+    this.wordLength = wordToGuess.length;
+    console.log('word length is ' + this.wordLength)
+    this.newLetterObjects = []
+    this.showGuessesArray = []
+    
+    for (let i = 0; i < this.wordLength; i++) {
+        var letter = new Letter(this.word[i]);
+        this.newLetterObjects.push(letter);
+    };
+
+    this.createWordArray = function (){
+        console.log("in create word array function");
+        // make the array by looping through the word;
+        for (i = 0; i < this.wordLength; i++) {
+            showGuessesArray += this.newLetterObjects[i] + " ";
+            }
+            console.log(showGuessesArray + "\n")
+    }
+    
+    this.storeUserGuess = function (userInput){
+        for (i = 0; i < this.newLetterObjects.length; i++) {
+            this.newLetterObjects[i].guess(input);
+            
+        }
+    };
+};
 
 //   * A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
 
+
 //   * A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
 
-const Letter = require('./letter')
-
-const Word = function (word){
-    this.word = word;
-    this.createWordArray = function (){
-        for (let i = 0; i < word.length; i++) {
-            // make the array by looping through the word;
-            
-        }
-    }
-}
+module.exports = Word;
